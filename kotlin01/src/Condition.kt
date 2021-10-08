efe0129caf969b687db0c0566c9f38638d6fc683
chlbupt <1032326058@qq.com>
2021-10-08 23:55:52 +0800

    first commit

---
+++ kotlin01/src/Condition.kt
@@ -0,0 +1,27 @@
+fun main() {
+    val age = 4
+
+    /*if (age in 0..3) {
+        println("婴幼儿")
+    } else if (age in 3..12) {
+        println("少儿")
+    } else {
+        println("未知")
+    }
+
+    if (age !in 1..3) {
+        println("少侠")
+    }*/
+
+    val school = "11"
+    val level = when (school) {
+        "学前班" -> "幼儿"
+        "小学" -> "少儿"
+        "中学" -> "青少年"
+        else -> {
+            println("未知")
+        }
+    }
+
+    println(level)
+}
---
+++ kotlin01/src/Hello.kt
@@ -0,0 +1,8 @@
+/*
+const val MAX = 200
+
+fun main() {
+//    var a: Int = 5;
+    var a = "hello"
+    println(a)
+}*/
